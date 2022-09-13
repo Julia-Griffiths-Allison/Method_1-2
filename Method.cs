b4633e088390cadd9bb8df5f8b5c6d8b876814d7
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-13 11:55:13 -0400

    failed my first repository, creating a new GitHub repo and moving my completed assignment to the new Repo

---
+++ Method.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    internal class Method
+    {
+        public static int Add(int one, int two)
+        {
+            return one + two;
+        }
+
+        public static int Subtract(int one, int two)
+        {
+            return two - one;
+        }
+
+        public static int Multiply(int one, int two)
+        {
+            return one * two;
+        }
+
+        public static int Divide(int one, int two)
+        {
+            return two / one;
+        }
+
+        public static int Module(int two, int five)
+        {
+            return five % two;
+        }
+    }
+}
